33b203b74f2cc80d2bfeb7ee10355eb4df429965
Ankan Sen <maharajankan@gmail.com>
2023-10-30 13:10:50 +0530

    Ankan

--- src/components/Teams.js
+++ src/components/Teams.js
@@ -43,7 +43,7 @@ const Teams = () => {
                     />
                     <div className="proj-txtx">
                       <h4>Prof Shirish H Sonawane</h4>
-                      <span>HOD-CII</span>
+                      <span>Head, CII</span>
                       <div
                         style={{
                           display: "flex",
